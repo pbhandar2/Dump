05d165651bc4ab68c5c048c035179022f56082ce
Pranav Bhandari <pbhandar2@users.noreply.github.com>
2020-07-02 03:06:59 -0400

    remove sudo and add -y flag

--- FAST21/deathstar_setup.sh
+++ FAST21/deathstar_setup.sh
@@ -1,7 +1,7 @@
-sudo apt-get update 
+apt-get update 
 
 ## Docker setup ##
-sudo apt-get install \
+apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -10,23 +10,23 @@ sudo apt-get install \
     
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo add-apt-repository \
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
    
-sudo apt-get update 
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+apt-get update 
+apt-get install -y docker-ce docker-ce-cli containerd.io
 
 ## Docker Compose setup ##
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 ## libssl-dev, libz-dev, luarocks, luasocket setup ##
-sudo apt-get install libssl-dev
-sudo apt-get install libz-dev
-sudo apt-get install luarocks
-sudo luarocks install luasocket
+apt-get install libssl-dev
+apt-get install libz-dev -y
+apt-get install luarocks -y
+luarocks install luasocket 
 
 ## 'make' might not be installed, although not in the repo guideline ##
-sudo apt install make 
+apt install make 
